8cde419763efc9c1928fad6c22734587698690c3
nahian28 <nahiansylhet@gmail.com>
2025-09-11 16:42:49 +0600

    Updates

--- includes/admin-submission-view.php
+++ includes/admin-submission-view.php
@@ -3,7 +3,7 @@ if (!defined('ABSPATH')) exit;
 
 function ucf7e_render_submission_view() {
     if (!current_user_can('manage_options')) {
-        wp_die(__('You are not allowed to access this page.','nahian-ultimate-cf7-elementor'));
+        wp_die(__('You are not allowed to access this page.', 'nahian-ultimate-cf7-elementor'));
     }
 
     // Load real submissions
@@ -11,7 +11,7 @@ function ucf7e_render_submission_view() {
     $index = isset($_GET['submission_index']) ? intval($_GET['submission_index']) : -1;
 
     if (!isset($submissions[$index])) {
-        echo '<div class="notice notice-error"><p>'.esc_html__('Submission not found.','nahian-ultimate-cf7-elementor').'</p></div>';
+        echo '<div class="notice notice-error"><p>' . esc_html__('Submission not found.', 'nahian-ultimate-cf7-elementor') . '</p></div>';
         return;
     }
 
@@ -24,30 +24,39 @@ function ucf7e_render_submission_view() {
     $submitted_at = !empty($s['submitted_at']) ? strtotime($s['submitted_at']) : 0;
     $date = $submitted_at ? date_i18n('j F Y', $submitted_at) : '';
     $time = $submitted_at ? date_i18n('g:ia', $submitted_at) : '';
-    ?>
 
+    // Optional: Define mapping of field keys to CF7 labels
+    $labels = isset($s['labels']) && is_array($s['labels']) ? $s['labels'] : [];
+    ?>
     <div class="wrap ucf7-wrap">
-        <h1><?php echo esc_html($form_title ?: __('Untitled Form','nahian-ultimate-cf7-elementor')); ?> - <?php esc_html_e('Submission Details','nahian-ultimate-cf7-elementor'); ?></h1>
+        <h1><?php echo esc_html($form_title ?: __('Untitled Form', 'nahian-ultimate-cf7-elementor')); ?> - <?php esc_html_e('Submission Details', 'nahian-ultimate-cf7-elementor'); ?></h1>
         <table class="widefat striped">
             <tbody>
                 <?php if (!empty($data)): ?>
-                    <?php foreach($data as $key=>$value): ?>
+                    <?php foreach ($data as $key => $value): ?>
                         <tr>
-                            <th><?php echo esc_html(ucfirst(str_replace('_',' ',$key))); ?></th>
+                            <th>
+                                <?php
+                                // Use the label if available, otherwise convert key to readable text
+                                echo esc_html($labels[$key] ?? ucfirst(str_replace('_', ' ', $key)));
+                                ?>
+                            </th>
                             <td><?php echo esc_html($value ?? ''); ?></td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else: ?>
-                    <tr><td colspan="2"><?php esc_html_e('No submission data available.','nahian-ultimate-cf7-elementor'); ?></td></tr>
+                    <tr>
+                        <td colspan="2"><?php esc_html_e('No submission data available.', 'nahian-ultimate-cf7-elementor'); ?></td>
+                    </tr>
                 <?php endif; ?>
                 <tr>
-                    <th><?php esc_html_e('Submitted At','nahian-ultimate-cf7-elementor'); ?></th>
+                    <th><?php esc_html_e('Submitted At', 'nahian-ultimate-cf7-elementor'); ?></th>
                     <td><?php echo esc_html($date . ' ' . $time); ?></td>
                 </tr>
             </tbody>
         </table>
         <p>
-            <a href="<?php echo esc_url(add_query_arg(['page'=>'ucf7e-submissions'], admin_url('admin.php'))); ?>" class="button"><?php esc_html_e('Back to Submissions','nahian-ultimate-cf7-elementor'); ?></a>
+            <a href="<?php echo esc_url(add_query_arg(['page' => 'ucf7e-submissions'], admin_url('admin.php'))); ?>" class="button"><?php esc_html_e('Back to Submissions', 'nahian-ultimate-cf7-elementor'); ?></a>
         </p>
     </div>
-<?php } 
+<?php }
